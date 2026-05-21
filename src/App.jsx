7de6fbdbcd5b94649991c2dd57919057693f042e
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-05-21 14:22:59 +0100

    set up tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import './App.css'
-
 function App() {
-
   return (
     <>
+      <h1 className="text-4xl font-bold text-blue-600">Tailwind is working!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
