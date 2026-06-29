f3a16b49674ba8d01e041296fb295c58cfa4a29a
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-06-29 20:21:00 +0100

    add styling to BrandingCard

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,13 +1,15 @@
 import LoginCard from "./LoginCard";
+import BrandingCard from "./BrandingCard";
 
 export default function Login() {
-
   return (
     <div className="flex">
-    <div className="w-[45%]"></div>
-    <div className="flex flex-col justify-center items-center min-h-[100vh] gap-y-4 w-[55%] bg-white py-4">
-      <LoginCard />
-    </div>
+      <div className="w-[45%] bg-violet-50 flex justify-start pl-[60px]">
+        <BrandingCard />
+      </div>
+      <div className="flex flex-col justify-center items-center min-h-[100vh] gap-y-4 w-[55%] bg-white py-4">
+        <LoginCard />
+      </div>
     </div>
   );
 }
