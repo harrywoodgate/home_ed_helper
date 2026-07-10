ebeef881b699d32028e5674367ad1e607f6736b2
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-07-10 18:14:54 +0100

    add styling to login screen for smaller screens

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -5,7 +5,7 @@ export default function Signup() {
 
   return (
     <div className="flex">
-      <div className="hidden w-[45%] bg-violet-50 sm:flex justify-start px-10 lg:pl-[60px]">
+      <div className="hidden w-[45%] bg-violet-50 sm:flex justify-start xl:justify-center px-10 lg:pl-[60px]">
         <SingupBrandingCard />
       </div>
       <div className="flex flex-col justify-center items-center min-h-[100vh] gap-y-4 w-full sm:w-[55%] bg-white px-4 py-4">
