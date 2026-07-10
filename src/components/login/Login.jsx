e8a335a7154521ffe88978803819cc11fb49083c
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-07-10 18:20:45 +0100

    edit screen height in login and signup so that they center properly on phone screens

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -7,7 +7,7 @@ export default function Login() {
       <div className="hidden w-[45%] bg-violet-50 sm:flex xl:justify-center px-10 lg:pl-[60px]">
         <LoginBrandingCard />
       </div>
-      <div className="flex flex-col justify-center items-center min-h-screen sm:gap-y-4 w-full sm:w-[55%] bg-white px-4 py-4">
+      <div className="flex flex-col justify-center items-center min-h-[100svh] sm:min-h-screen sm:gap-y-4 w-full sm:w-[55%] bg-white px-4 py-4">
         <LoginCard />
       </div>
     </div>
