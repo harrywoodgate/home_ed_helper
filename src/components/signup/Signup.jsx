9f70e83da6f585dd874d5a4bb699f606940ee914
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-07-10 18:16:10 +0100

    add styling so signup screen for smaller screen sizes

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -8,7 +8,7 @@ export default function Signup() {
       <div className="hidden w-[45%] bg-violet-50 sm:flex justify-start xl:justify-center px-10 lg:pl-[60px]">
         <SingupBrandingCard />
       </div>
-      <div className="flex flex-col justify-center items-center min-h-[100vh] gap-y-4 w-full sm:w-[55%] bg-white px-4 py-4">
+      <div className="flex flex-col justify-center items-center min-h-screen sm:gap-y-4 w-full sm:w-[55%] bg-white px-4 py-4">
         <SignUpCard />
       </div>
     </div>
