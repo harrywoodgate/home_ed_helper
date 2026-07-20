1bf89183ee30e8a2238648614762f791fdb3fe98
Mrsguigles11 <harry_woodgate11@hotmail.co.uk>
2026-07-20 19:51:44 +0100

    add pageSelector to History

---
+++ src/components/dashboard/history/HistoryReport.jsx
@@ -0,0 +1,74 @@
+import { downloadPdf } from "../../../utils/downloadPdf";
+import { previewPdf } from "../../../utils/previewPdf";
+import { fetchBlob } from "../../../utils/fetchBlob";
+
+export default function HistoryReport({ report, setDeleteFile }) {
+  return (
+    <div
+      className="flex flex-wrap px-6 py-4 sm:py-0 min-h-[106px] gap-x-2 items-center justify-between mt-1 bg-white rounded-2xl border shadow-[0_1px_3px_rgba(15,23,42,0.05),_0_8px_24px_rgba(15,23,42,0.04)] gap-y-6"
+      key={report.id}
+    >
+      <div className="flex items-center sm:gap-x-6 w-full sm:w-auto">
+        <h3 className="font-semibold w-[50%] sm:w-[130px] text-center">
+          {report.file_name.split("-")[0]}
+        </h3>
+        <div className="h-[50px] w-[2px] bg-border"></div>
+        <p className="w-[50%] sm:w-auto text-center">
+          {report.file_name.split("-")[1]}
+        </p>
+      </div>
+      <div className="flex w-full sm:w-auto justify-center flex-wrap gap-x-6 gap-y-4">
+        <button
+          className="flex items-center gap-x-2 sm:gap-x-3 text-xs sm:text-sm"
+          onClick={async () => {
+            const blob = await fetchBlob(report.file_path);
+            previewPdf(blob);
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-4 h-4 sm:w-5 sm:h-5"
+          >
+            <title>preview</title>
+            <path d="M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9M12,4.5C17,4.5 21.27,7.61 23,12C21.27,16.39 17,19.5 12,19.5C7,19.5 2.73,16.39 1,12C2.73,7.61 7,4.5 12,4.5M3.18,12C4.83,15.36 8.24,17.5 12,17.5C15.76,17.5 19.17,15.36 20.82,12C19.17,8.64 15.76,6.5 12,6.5C8.24,6.5 4.83,8.64 3.18,12Z" />
+          </svg>
+          <span>Preview</span>
+        </button>
+        <button
+          onClick={() => downloadPdf(report.file_path, report.file_name)}
+          className="flex items-center gap-x-2 sm:gap-x-3 text-xs sm:text-sm"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-4 h-4 sm:w-5 sm:h-5"
+          >
+            <title>download</title>
+            <path d="M2 12H4V17H20V12H22V17C22 18.11 21.11 19 20 19H4C2.9 19 2 18.11 2 17V12M12 15L17.55 9.54L16.13 8.13L13 11.25V2H11V11.25L7.88 8.13L6.46 9.55L12 15Z" />
+          </svg>
+          <span>Download</span>
+        </button>
+        <button
+          className="flex items-center gap-x-2 sm:gap-x-3 text-xs sm:text-sm text-red-500"
+          onClick={() => {
+            setDeleteFile({
+              filePath: report.file_path,
+              fileName: report.file_name,
+            });
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="w-4 h-4 sm:w-5 sm:h-5 fill-red-500"
+          >
+            <title>delete</title>
+            <path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M7,6H17V19H7V6M9,8V17H11V8H9M13,8V17H15V8H13Z" />
+          </svg>
+          Delete
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/dashboard/history/PageSelector.jsx
@@ -0,0 +1,37 @@
+
+export default function PageSelector({ pageNumber, setPageNumber, numberOfPages }) {
+
+  return (
+    <div className="w-full flex justify-end gap-x-2 mt-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        className="w-6 fill-secondary_text cursor-pointer"
+        onClick={() => {
+          if (pageNumber === 1) {
+            return;
+          }
+          setPageNumber((prev) => prev - 1);
+        }}
+      >
+        <title>page-left</title>
+        <path d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
+      </svg>
+      <div className="flex justify-center items-center bg-darker_background w-8 h-8 text-secondary rounded-md border-border border-2 pr-[1px]">{pageNumber}</div>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        className="w-6 fill-secondary_text cursor-pointer"
+        onClick={() => {
+          if (pageNumber === numberOfPages) {
+            return;
+          }
+          setPageNumber((prev) => prev + 1);
+        }}
+      >
+        <title>page-right</title>
+        <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+      </svg>
+    </div>
+  );
+}
